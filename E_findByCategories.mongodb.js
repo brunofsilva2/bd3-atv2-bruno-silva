c6976b450723a73fe97eb666c8032da9ae8b2c6f
brunofsilva2 <bruno.19fs@gmail.com>
2024-09-30 21:44:09 -0300

    finalizando a atividade de banco de dados 3

---
+++ E_findByCategories.mongodb.js
@@ -0,0 +1,12 @@
+const database = 'BD3-BRUNO-AULA';
+
+const collection = 'BD3_ATV2_PRODUTOS';
+
+use(database);
+
+db[collection].find({
+    $or: [
+        {categoria: "Antibiótico"},
+        {categoria: "Controlado"}
+    ]
+})
